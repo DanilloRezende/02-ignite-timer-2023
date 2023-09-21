a30356502bdc252b733f553f1a9cf01fe17ffd5a
DanilloRezende <danillo.rezende3@gmail.com>
2023-09-21 08:04:53 -0300

    First Commit

---
+++ 02-ignite-timer/src/App.tsx
@@ -0,0 +1,20 @@
+import { ThemeProvider } from 'styled-components'
+import { BrowserRouter } from 'react-router-dom'
+import { defaultTheme } from './styles/themes/default'
+import { GlobalStyles } from './styles/global'
+import { Router } from './Router'
+
+function App() {
+
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router/>
+
+        <GlobalStyles />
+      </BrowserRouter>
+    </ThemeProvider>
+  )
+}
+
+export default App
